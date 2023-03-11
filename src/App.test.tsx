265e4a422d99bc3dc22d9a2dd513982305c50238
David Blankenship <dblanken@iu.edu>
2023-03-10 21:00:46 -0500

    Write an actual good test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders card title element', () => {
   render(<App />);
   const titleElement = screen.getByText(/Darglot/i);
   expect(titleElement).toBeInTheDocument();
+});
+
+test('can find a card', () => {
+  render(<App />);
+  fireEvent.change(screen.getByLabelText(/Filter by:/i), {target: { value: 'dar' }});
+  const darglotElement = screen.getByText(/Darglot/i);
+  const shilohElement = screen.queryByText(/Shiloh/i);
+
+  expect(darglotElement).toBeInTheDocument();
+  expect(shilohElement).not.toBeInTheDocument();
 });
