322952f5c7a66de8d1faa5a9718f3cf8b15fd330
David Blankenship <dblanken@iu.edu>
2023-03-16 15:18:42 -0400

    Add tests for Song

---
+++ src/components/Song.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Song from './Song';
+import { warn } from 'console';
+
+test('renders a song', () => {
+  const songItem: SongItem = {
+    name: 'blah',
+    copyrightYear: 2023,
+    coverart: './blah.jpg',
+    length: {
+      minutes: 5,
+      seconds: 5
+    },
+    link: "https://localhost",
+    yearWritten: 2022
+  }
+
+  render(<Song song={songItem} />);
+
+  const songNameElement = screen.getByText(songItem.name);
+  expect(songNameElement).toBeInTheDocument();
+
+  const songLength = screen.getByText(`${songItem.length.minutes}:${songItem.length.seconds}`);
+  expect(songLength).toBeInTheDocument();
+
+  const songCopyrightYear = screen.getByText(new RegExp(songItem.copyrightYear.toString(), 'i'));
+  expect(songCopyrightYear).toBeInTheDocument();
+
+  const songYearWritten = screen.getByText(new RegExp(songItem.yearWritten.toString(), 'i'));
+  expect(songYearWritten).toBeInTheDocument();
+
+  const songCoverart = screen.getByAltText(songItem.name);
+  expect(songCoverart).toBeInTheDocument();
+  expect(songCoverart).toHaveAttribute('src');
+  expect(songCoverart.getAttribute('src')).toEqual(songItem.coverart);
+
+  const songLink = screen.getByText("Listen");
+  expect(songLink).toBeInTheDocument();
+  expect(songLink.getAttribute('href')).toEqual(songItem.link);
+  // expect(songCoverart.getAttribute('href')).toEqual(songItem.coverart);
+});
